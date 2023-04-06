88f406b1afc95e95e6814cc71570309ff4c5b72a
Sema <123366660+Semafftp@users.noreply.github.com>
2023-04-06 21:28:28 +0700

    Add files via upload

---
+++ Class inheritance/cats.py
@@ -0,0 +1,8 @@
+class Cat:
+  
+   def __init__(self, name,kind,gender,species, age, ):
+       self.name = name
+       self.kind = kind
+       self.gender = gender
+       self.species = species
+       self.age = age
