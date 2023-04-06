aa9fa3dfbae59e1f85220c2e5be8501a1cc1b1a3
Sema <123366660+Semafftp@users.noreply.github.com>
2023-04-06 21:22:37 +0700

    Add files via upload

---
+++ beta/cats.py
@@ -0,0 +1,13 @@
+class Cat:
+    #Имя: Барон
+#Вид животного: кошка
+#Пол: мальчик
+#Порода: сибирская кошка
+#Возраст: 2 года
+#Регистрационный документ: с29022020
+   def __init__(self, name,kind,gender,species, age, ):
+       self.name = name
+       self.kind = kind
+       self.gender = gender
+       self.species = species
+       self.age = age
