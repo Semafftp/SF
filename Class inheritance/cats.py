1f7572932e9aa27b13e62aa9842d62d56e0126f1
Sema <123366660+Semafftp@users.noreply.github.com>
2023-04-06 21:31:42 +0700

    Delete cats.py

--- Class inheritance/cats.py
+++
@@ -1,8 +0,0 @@
-class Cat:
-  
-   def __init__(self, name,kind,gender,species, age, ):
-       self.name = name
-       self.kind = kind
-       self.gender = gender
-       self.species = species
-       self.age = age
